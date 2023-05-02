06ce7e739ba1ae2848ce2c1c88f10caec5b7e2d1
Vivek Kumar <vivek.26@outlook.com>
2023-05-02 13:30:59 +0530

    fetch and show badges from shields.io

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 mod datastore;
 mod state;
 
-use axum::{extract::State as StateExtractor, response::Html, routing::get, Router};
+use anyhow::Error;
+use axum::{
+    body::Bytes,
+    extract::State as StateExtractor,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
 use datastore::PostgresDB;
 use dotenv::dotenv;
+use reqwest::header::{HeaderMap, HeaderValue};
 use state::State;
 use std::net::SocketAddr;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
@@ -47,7 +56,50 @@ async fn main() -> Result<(), anyhow::Error> {
     Ok(())
 }
 
-async fn handler(StateExtractor(state): StateExtractor<State<PostgresDB>>) -> Html<String> {
+async fn handler(StateExtractor(state): StateExtractor<State<PostgresDB>>) -> Response {
     let views = state.update();
-    Html(format!("<h1>Profile views = {}</h1>", views))
+    let url = format!(
+        "https://shields.io/static/v1?label=Profile%20Views&message={}&color=brightgreen",
+        views
+    );
+
+    match fetch_badge(&url).await {
+        Ok(badge) => (
+            // docs - https://docs.rs/axum/latest/axum/response/index.html
+            StatusCode::OK,
+            [
+                (
+                    "Cache-Control",
+                    "max-age=0, no-cache, no-store, must-revalidate",
+                ),
+                ("Content-Type", "image/svg+xml"),
+            ],
+            badge,
+        )
+            .into_response(),
+        Err(e) => {
+            tracing::error!("failed to fetch badge from shields.io, reason: {}", e);
+            StatusCode::INTERNAL_SERVER_ERROR.into_response()
+        }
+    }
+}
+
+async fn fetch_badge(url: &str) -> Result<Bytes, Error> {
+    tracing::info!("badge url: {}", url);
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        "Cache-Control",
+        HeaderValue::from_static("max-age=0, no-cache, no-store, must-revalidate"),
+    );
+
+    let client = reqwest::Client::new();
+    let response = client
+        .get(url)
+        .headers(headers)
+        .send()
+        .await?
+        .bytes()
+        .await?;
+
+    Ok(response)
 }
